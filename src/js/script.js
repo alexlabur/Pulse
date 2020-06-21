76b5ef07de7618d0b1fe9b45056dc56323869fcd
Alex <deicider84@mail.ru>
2020-06-21 14:35:45 +0300

    slider

--- src/js/script.js
+++ src/js/script.js
@@ -26,10 +26,20 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        adaptiveHeight: true,
+        // adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    arrows: false,
+                    dots: true
+                }
+
+            }
+        ]
         
-    })
+    });
 });
   
