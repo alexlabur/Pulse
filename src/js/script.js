6ff5addc7878977a789ff691085cd5ee7f77beb0
Alex <deicider84@mail.ru>
2020-07-24 15:52:06 +0300

    adapt

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,37 @@
 
 
 
-// $(document).ready(function(){
-//     $('.carousel__inner').slick({
-//         speed: 1200,
-//         adaptiveHeight: true,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-//         responsive: [
-//             {
-//                 breakpoint: 992,
-//                 settings: {
-//                     arrows: false,
-//                     dots: true
-//                 }
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    arrows: true,
+                    dots: false
+                },
 
-//             }
-//         ]
+				breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    dots: true
+				},
 				
-//     });
-// });
+				breakpoint: 576,
+                settings: {
+                    arrows: false,
+                    dots: true
+                }
+			}
+			
+        ]
+				
+    });
+});
 	
 const slider = tns({
 		container: '.carousel__inner',
