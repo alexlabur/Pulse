53a0b1c600bfc03da9908e7df5ee136414bfd2de
Alex <deicider84@mail.ru>
2020-07-27 21:32:24 +0300

    adapt

--- src/js/script.js
+++ src/js/script.js
@@ -9,20 +9,13 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [
             {
-                breakpoint: 992,
-                settings: {
-                    arrows: true,
-                    dots: false
-                },
-			},
-			{	
 				breakpoint: 768,
                 settings: {
                     arrows: false,
                     dots: true
 				},
-			},
-			{
+            },
+            {
 				breakpoint: 576,
                 settings: {
                     arrows: false,
@@ -35,22 +28,7 @@ $(document).ready(function(){
     });
 });
 	
-// const slider = tns({
-// 		container: '.carousel__inner',
-// 		items: 1,
-// 		slideBy: 'page',
-// 		autoplay: false,
-// 		controls: false,
-// 		nav: false
-// 	});
 
-// 		document.querySelector('.prev').addEventListener('click', function () {
-// 		slider.goTo('prev');
-// 	});
-
-// 			document.querySelector('.next').addEventListener('click', function () {
-// 		slider.goTo('next');
-// 	});
 
 	
 
