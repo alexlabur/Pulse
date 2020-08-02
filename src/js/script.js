54497efb942b48cf9ce7997bf018980f692af9a4
Alex <deicider84@mail.ru>
2020-08-02 15:16:34 +0300

    dots off

--- src/js/script.js
+++ src/js/script.js
@@ -3,23 +3,32 @@
 
 $(document).ready(function(){
     $('.carousel__inner').slick({
-        speed: 1200,
+		dots: false,
+		speed: 1200,
+		slidesToShow: 1,
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [
 			{
+				breakpoint: 1200,
+                settings: {
+                    arrows: false,
+                    dots: false
+				},
+            },
+			{
 				breakpoint: 992,
                 settings: {
                     arrows: false,
-                    dots: true
+                    dots: false
 				},
             },
             {
 				breakpoint: 768,
                 settings: {
                     arrows: false,
-                    dots: true
+                    dots: false
 				},
 
             },
@@ -27,7 +36,7 @@ $(document).ready(function(){
 				breakpoint: 576,
                 settings: {
                     arrows: false,
-                    dots: true
+                    dots: false
                 }
 			}
 			
